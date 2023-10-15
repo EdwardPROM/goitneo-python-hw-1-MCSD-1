849db55d85672ebf7c48b95c3a15aaffcbfc964e
EdwardProm <eduardkoles@gmail.com>
2023-10-15 15:22:48 +0300

    1.0

---
+++ task1.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+def get_birthdays_per_week(users):
+    users = defaultdict(list)
+    current_date = datetime.today().date() # Отримуємо поточну дату системи для подальшого порівняння з датами народження користувачів
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()  # Конвертуємо час народження до типу date, видаляючи часову частину.
+        birthday_this_year = birthday.replace(year=today.year) # Рядок використовує метод replace() для заміни значення року у даті на значення поточного року (today.year).
+        if  birthday_this_year < today(): #Перевіряємо, чи вже минув день народження цього року
+            delta_days = (birthday_this_year - today).days. # Якщо так, то розглядаємо дату на наступний рік, це треба у birthday_this_year використовуючи метод replace
+            # збільшити рік на одиничку.
+
+
+
+
